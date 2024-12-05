134e9da603046c777fe39b5857fce042bd61a133
Auroraxr-cyber <auroraxr8.06@gmail.com>
2024-12-05 11:38:54 +0800

    景點VR

---
+++ 火影忍者VR/data.js
@@ -0,0 +1,237 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--ninja-totolo",
+      "name": "宜蘭忍者村 NINJA TOTOLO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1408,
+      "initialViewParameters": {
+        "yaw": 2.744611863296665,
+        "pitch": -0.03061984991661859,
+        "fov": 0.8280972189554987
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8945070366842565,
+          "pitch": 0.005962630789639434,
+          "rotation": 6.283185307179586,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6723018801217666,
+          "pitch": 0.0022426863248110607,
+          "title": "宜蘭忍者村NINJA&nbsp; TOTOLO",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "日本富士山急樂園火影忍者×博人富士木叶隐村",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.9587578542025543,
+        "pitch": 0.0020743121334980685,
+        "fov": 0.7881502416490269
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7659035827770033,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3087459768885825,
+          "pitch": 0.5772186925671932,
+          "title": "燒肉Q",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.2471223915926526,
+          "pitch": 0.30374185164030365,
+          "title": "一樂拉麵",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.9786273722950067,
+          "pitch": 0.0634086072826392,
+          "title": "日本富士山急樂園火影忍者×博人富士木葉隱村",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "蝦墓入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.2956001393019534,
+        "pitch": 0.025424078492388702,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2163399729396165,
+          "pitch": 0.07264107876704884,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.821437706753084,
+          "pitch": -0.05016996624593162,
+          "title": "蝦墓入口",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "蝦墓排隊處",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.0145090715649783,
+        "pitch": 0.023253260057874314,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0672352236582352,
+          "pitch": 0.27366538834002085,
+          "rotation": 0,
+          "target": "4-----"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0910303134221486,
+          "pitch": 0.058415526706767196,
+          "title": "蝦墓排隊處",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-----",
+      "name": "蝦墓 ( 火影忍者主題樂園 '忍里' )",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.013280655746936,
+        "pitch": 0.03360550527179029,
+        "fov": 0.8127072035521081
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 3.0428519547767596,
+          "pitch": 0.04054205173373582,
+          "title": "蝦墓火影岩",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
