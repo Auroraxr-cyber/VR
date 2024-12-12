d08eb02b7c5dff999f50f75cbdd550104423b238
Auroraxr-cyber <auroraxr8.06@gmail.com>
2024-12-12 12:00:41 +0800

    NarutoVR2

---
+++ NarutoVR2/app-files/data.js
@@ -0,0 +1,443 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "日本富士山急樂園火影忍者×博人富士木葉隐村大門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.6876776339515,
+        "pitch": 0.15138942283386925,
+        "fov": 0.7881502416490269
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7808340661576736,
+          "pitch": 0.14739654341602026,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7011588421871586,
+          "pitch": 0.09599380648284317,
+          "title": "木葉隱村大門",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "鳴人",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -3.0398827052561117,
+        "pitch": 0.08344668352680529,
+        "fov": 0.7881502416490269
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9951162614816944,
+          "pitch": 0.13512272515886004,
+          "rotation": 3.9269908169872414,
+          "target": "2-q"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0000681956422106,
+          "pitch": 0.030943172485292436,
+          "title": "鳴人",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-q",
+      "name": "燒肉Q",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.2812282324217001,
+        "pitch": 0.24509178927050357,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1504776264048537,
+          "pitch": 0.36243097521866474,
+          "rotation": 10.995574287564278,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2384697202936383,
+          "pitch": 0.10770169484629477,
+          "title": "燒肉Q",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "一樂拉麵",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.40811876818890624,
+        "pitch": 0.16994272536547683,
+        "fov": 1.130482167926661
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1101369503602374,
+          "pitch": 0.26506024997403443,
+          "rotation": 4.71238898038469,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.23928174314605677,
+          "pitch": 0.30335035879434,
+          "title": "一樂拉麵",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "居酒屋",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.514067095645607,
+        "pitch": -0.00578241138147817,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 3.039276142872838,
+          "pitch": 0.21967773079183317,
+          "title": "居酒屋",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-q",
+      "name": "燒鳥Q",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.1981111101038717,
+        "pitch": 0.04316479562431397,
+        "fov": 0.9587296632400657
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3409957335594171,
+          "pitch": 0.18196001848199117,
+          "rotation": 0,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0386999955089422,
+          "pitch": 0.17227372423212728,
+          "title": "燒鳥Q<div><br></div>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-",
+      "name": "蝦墓入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.276437749832648,
+          "pitch": 0.08399924978496642,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9969520123839537,
+          "pitch": 0.026081092062794298,
+          "title": "蝦墓入口",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-",
+      "name": "蝦墓排隊處",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.1166851838137788,
+        "pitch": 0.1934076262745723,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0733833637185235,
+          "pitch": 0.4363514591845181,
+          "rotation": 0.7853981633974483,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3556623035898117,
+          "pitch": 0.40188155439538775,
+          "title": "入口",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-",
+      "name": "蝦墓火影岩",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.1308595993028074,
+        "pitch": 0.05957215377192604,
+        "fov": 0.7731766777904946
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.066758081570363,
+          "pitch": 0.0413969037861861,
+          "rotation": 0,
+          "target": "9--ninja-totolo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9235189177353718,
+          "pitch": 0.01647451087104379,
+          "title": "蝦墓火影岩",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "9--ninja-totolo",
+      "name": "宜蘭忍者村 NINJA TOTOLO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1408,
+      "initialViewParameters": {
+        "yaw": 2.7047296016005564,
+        "pitch": -0.06194170240299357,
+        "fov": 0.8280972189554987
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.730311037416822,
+          "pitch": -0.02671175913261692,
+          "title": "宜蘭忍者村 NINJA TOTOLO",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
